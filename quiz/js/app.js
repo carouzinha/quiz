a91ae006ec1ea19683c7fdd4f76a17acdf250fb7
Caroline Rodrigues <123772397+carouzinha@users.noreply.github.com>
2023-08-16 13:40:47 -0300

    Add files via upload

---
+++ quiz/js/app.js
@@ -0,0 +1,61 @@
+const resultado1 = document.getElementById("resultado1")
+function verificacao1(resposta1){
+    if(resposta1 === 'a'){
+        resultado1.innerHTML = ('Resposta correta');
+        resultado1.style.color='green'
+        resultado1.style.width='300px'
+        resultado1.style.height='50px'
+        resultado1.style.backgroundColor='rgba(137, 43, 226, 0.361)'
+        resultado1.style.borderRadius='100px'
+        resultado1.style.alignItems='center'
+    }else{
+        resultado1.innerHTML = ('Resposta incorreta');
+        resultado1.style.color='red'
+        resultado1.style.width='300px'
+        resultado1.style.height='50px'
+        resultado1.style.backgroundColor='rgba(137, 43, 226, 0.361)'
+        resultado1.style.borderRadius='100px'
+        resultado1.style.alignItems='center'
+    }
+}
+const resultado2 = document.getElementById("resultado2")
+function verificacao2(resposta2){
+    if(resposta2 === 'a'){
+        resultado2.innerHTML = ('Resposta correta');
+        resultado2.style.color='green'
+        resultado2.style.width='300px'
+        resultado2.style.height='50px'
+        resultado2.style.backgroundColor='rgba(137, 43, 226, 0.361)'
+        resultado2.style.borderRadius='100px'
+        resultado2.style.alignItems='center'
+    }else{
+        resultado2.innerHTML = ('Resposta incorreta');
+        resultado2.style.color='red'
+        resultado2.style.width='300px'
+        resultado2.style.height='50px'
+        resultado2.style.backgroundColor='rgba(137, 43, 226, 0.361)'
+        resultado2.style.borderRadius='100px'
+        resultado2.style.alignItems='center'
+    }
+}
+const resultado3 = document.getElementById("resultado3")
+function verificacao3(resposta3){
+    if(resposta3 === 'a'){
+        resultado3.innerHTML = ('Resposta correta');
+        resultado3.style.color='green'
+        resultado3.style.width='300px'
+        resultado3.style.height='50px'
+        resultado3.style.backgroundCorgbargba ='rgba(137, 43, 226, 0.361)'
+        resultado3.style.borderRadius='100px'
+        resultado3.style.alignItems='center'
+    }else{
+        resultado3.innerHTML = ('Resposta incorreta');
+        resultado3.style.color='red'
+        resultado3.style.width='300px'
+        resultado3.style.height='50px'
+        resultado3.style.backgroundColor='rgba(137, 43, 226, 0.361)'
+        resultado3.style.borderRadius='100px'
+        resultado3.style.alignItems='center'
+
+    }
+}
